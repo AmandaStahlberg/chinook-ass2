f6a10946261980066c3999f9582ed8c843830f32
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-02-21 16:14:05 +0100

    first commit

---
+++ src/main/java/com/example/chinookass2/ChinookAss2Application.java
@@ -0,0 +1,13 @@
+package com.example.chinookass2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookAss2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinookAss2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chinookass2/ChinookAss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinookass2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookAss2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
