0dd0d80d3dc0c313d5dbb551c247b47f4186a060
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-23 00:42:45 +0100

    feat: add Most Popular Genre to main

--- src/main/java/com/example/chinookass2/runners/ApplicationRunner.java
+++ src/main/java/com/example/chinookass2/runners/ApplicationRunner.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 @Component
 public class ApplicationRunner implements org.springframework.boot.ApplicationRunner {
 
@@ -13,10 +16,38 @@ public class ApplicationRunner implements org.springframework.boot.ApplicationRu
     @Autowired
     public ApplicationRunner(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+
     }
+
+    public static void menu(String[] options){
+
+            for (String option : options){
+                System.out.println(option);
+            }
+            System.out.print("Choose an option 1-" + options.length + ": ");
+        }
+
+        public void getMostPopularGenre(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Give the customer an id: ");
+            boolean cont = true;
+            while (cont){
+                try {
+                    System.out.println(customerRepository.returngetCustomerMostPopularGenre(scanner.nextInt()).genre());
+                    cont = false;
+                }catch (InputMismatchException ex){
+                    System.out.print("the customer id should be a number: ");
+                    scanner.next();
+                }
+            }
+        }
+
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+
         // Requirement 1
         System.out.println(customerRepository.findAll());
         // Requirement 2, find customer with id 1
