d823fc26ea395d115953f3021cf286f2865aff86
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-23 01:03:53 +0100

    feat: Add menu and options to main

--- src/main/java/com/example/chinookass2/runners/ApplicationRunner.java
+++ src/main/java/com/example/chinookass2/runners/ApplicationRunner.java
@@ -47,7 +47,66 @@ public class ApplicationRunner implements org.springframework.boot.ApplicationRu
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        String[] options = {
+                "0. Exit",
+                "1. Read all customers",
+                "2. Search customer by the id",
+                "3. Search customer by the name",
+                "4. Read a page of customers",
+                "5. Add a new customer",
+                "6. Update an existing customer",
+                "7. find the country with most customers",
+                "8. find the highest spender",
+                "9. Show most popular genre for a given customer"
+        };
+        Scanner scanner = new Scanner(System.in);
+        int option=-1;
+        while (option!=0){
+            menu(options);
+            try {
+                option = scanner.nextInt();
+                switch (option){
+                    case 0:
+                        option=0;
+                        break;
+                    case 1:             // Print out all customers from the db
+                        customerRepository.findAll().stream().forEach(customer -> System.out.println(customer.toString()));
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
 
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                       case 7:
+                        System.out.println(customerRepository.returnCountryWithMostCustomers().country() + " (" +
+                                customerRepository.returnCountryWithMostCustomers().numberOfCustomers() + " customers)");
+                        break;
+                    case 8:
+                        System.out.println(
+                                customerRepository.findById(customerRepository.returngetHighestSpender().customerId()).firstName() +
+                                        " " + customerRepository.findById(customerRepository.returngetHighestSpender().customerId()).lastName() +
+                                        " (total: " + customerRepository.returngetHighestSpender().toString() + ")");
+                        break;
+                    case 9:
+                        getMostPopularGenre();
+                        break;
+                }
+            }catch (InputMismatchException ex){
+                System.out.println("Choose option: 1-" + options.length);
+                scanner.next();
+            }
+        }
+        //getMostPopularGenre();
         // Requirement 1
         System.out.println(customerRepository.findAll());
         // Requirement 2, find customer with id 1
